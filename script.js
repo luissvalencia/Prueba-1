eec3d5a42db37c6ad355e46ee3061c5772dc9d48
luissvalencia <129136909+luissvalencia@users.noreply.github.com>
2023-05-12 08:03:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const userIcon = document.getElementById('user-icon');
+const loginFormContainer = document.getElementById('login-form-container');
+
+userIcon.addEventListener('click', () => {
+  loginFormContainer.style.display = 'block';
+});
+
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  if (!target.closest('#user-icon') && !target.closest('#login-form-container')) {
+    loginFormContainer.style.display = 'none';
+  }
+});
+
+var images = document.querySelectorAll("#image-slider img");
+  var currentImageIndex = 0;
+  var interval = setInterval(nextImage, 5000);
+
+  function nextImage() {
+    images[currentImageIndex].classList.remove("active");
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+      currentImageIndex = 0;
+    }
+    images[currentImageIndex].classList.add("active");
+  }
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByTagName("img");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].className = "";
+    }
+    slides[slideIndex-1].className = "active";
+  }
+  
